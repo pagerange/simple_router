025e09b7cc56b5351e23d8e991ae1a6505140e1b
Steve George <steve@pagerange.com>
2018-09-13 07:09:21 -0500

    Added 404 handler

--- app/SimpleRouter.php
+++ app/SimpleRouter.php
@@ -11,6 +11,8 @@
 
 namespace App;
 
+use \App\View;
+
 class SimpleRouter
 {
 
@@ -47,7 +49,7 @@ class SimpleRouter
     static private $PATCH_routes = [];
 
     /**
-     *  Array of regexex and param names with route as key
+     *  Array of regexes and param names with route as key
      * @var array
      */
     static private $PATCH_regex = [];
@@ -131,25 +133,56 @@ class SimpleRouter
         $routes = "{$type}_routes";
         foreach(static::$$routes as $route => $value)
         {
+            // route with slashes trimmed
             $route = trim($route, '/');
+
+            // array of the route segments
             $segs = explode('/', $route);
+
+            // the regex that will match the route
             $reg = '';
+
+            // the params in each route
             $vars = [];
+
+            // define which segments are parameters
+            // and assign a simple regex to match them
             for($i = 0; $i< count($segs); $i++)
             {
+                // If the segment does not begin with
+                // a curly bracket in position zero
+                // it is NOT a parameter, and we can
+                // simply match it with a capture group
+                // that equals the segments string value
                 if(strpos($segs[$i], '{') !== 0) {
+                    // set the regex as a capture group
+                    // of the segment value
                     $reg .= '(' . $segs[$i] . ')';
                 } else {
+                    // if the segment DOES begin with
+                    // a curly bracket, it must be a param
+                    // So... trim the curly brackets and 
+                    // set a simple regex to match any param
+                    // value... (.+)
                     $index = trim($segs[$i], '{}');
                     $vars[$index] = $i + 1;
                     $reg .= '(.+)'; 
                 }
-
+                // we need to add a literal slash between segments in
+                // the regex, of course, to match slases in the URI
                 $reg .= '\/';
             }
+
+            // Trim any slashes from front and back
             $reg = trim($reg, '\/');
+
+            // Add the opening an ending anchors to the regex
             $reg = '/^' . $reg . '$/';
+
+            // Set the regex for a specific route
             static::$$regex[$route]['regex'] = $reg;
+
+            // Set the expected params for a sepcific route
             static::$$regex[$route]['vars'] = $vars;
 
         }
@@ -167,22 +200,40 @@ class SimpleRouter
         $routes = "{$type}_routes";
         $params = [];
         if($request == '/') {
+                // handle a request for the home page... 
                 return call_user_func_array(static::$$routes[$request], $params);
         } else {
+            // match the request against a regex
             foreach(static::$$regex as $key => $route)
             {
+                // trim slashes from request
                 $clean_request = trim($request, '/');
+
+                // attempt to match request agains regex for routes
                 preg_match($route['regex'], $clean_request, $matches);
+
+                // If it matches
                 if(isset($matches[0]) && $matches[0] == $clean_request) {
+                    // build the parameter array
                     foreach($route['vars'] as $index => $var) {
                         $params[$index] = $matches[$var];
                     }
+                    // call the method associated with the route
+                    // and pass it the params
                     return call_user_func_array(static::$$routes[$key], $params);
                 } 
             }
-        return call_user_func('error_404');
+        // else call the error 404 method
+        return View::show('error_404');
+
         }
 
+    }
+
+    // for testing only
+    static public function getRegex()
+    {
+        return self::$GET_regex;
     }
 
 }
---
+++ app/views/error_404.view.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=3">
+    <title>404 - Page Not FOund</title>
+</head>
+<body>
+
+    <h1>404 - Page Not Found</h1>
+
+    <p>Template: views/error_404.php</p>
+
+</body>
+</html>
